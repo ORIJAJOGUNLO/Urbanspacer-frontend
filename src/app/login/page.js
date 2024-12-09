740aa992faf0d7c5d547e4472b5f703fd36daf4e
blueprintsPathway <107635376+blueprintsPathway@users.noreply.github.com>
2024-12-09 17:07:25 +0100

    remove spaces

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import AuthLayout from '../components/AuthLayout'
-
-
+import React from "react";
+import AuthLayout from "../components/AuthLayout";
 
 const LoginPage = () => {
   return (
     <div>
       <AuthLayout>
         {/* Left Section with Background */}
-        <div className='flex-1'>
-          <div className='flex flex-col h-5/6 justify-center items-center'>
+        <div className="flex-1">
+          <div className="flex flex-col h-5/6 justify-center items-center">
             <h1 className="text-white text-8xl mt-24 font-bold px-8">
               Great to have you back!
             </h1>
@@ -18,11 +16,14 @@ const LoginPage = () => {
 
         {/* Right section with Background */}
         <div className="flex-1 flex items-center justify-center bg-red text-black">
-          <div className='bg-white w-1/2 flex flex-col px-16 rounded-2xl mt-12'>
-            <h1 className='my-6'>Sign in</h1>
+          <div className="bg-white w-1/2 flex flex-col px-16 rounded-2xl mt-12">
+            <h1 className="my-6">Sign in</h1>
             <form className="mt-6 space-y-4">
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
                   Email address
                 </label>
                 <input
@@ -35,7 +36,10 @@ const LoginPage = () => {
               {/* Password Input */}
               <div>
                 <div className="flex justify-between items-center">
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
                     Password
                   </label>
                   <a href="#" className="text-sm underline">
@@ -51,53 +55,60 @@ const LoginPage = () => {
 
               {/* Sign In Button */}
               <div>
-                <button type="submit" className="w-full mt-6 py-3 rounded-full px-4 bg-[rgba(33,33,33,1)] text-white">
+                <button
+                  type="submit"
+                  className="w-full mt-6 py-3 rounded-full px-4 bg-[rgba(33,33,33,1)] text-white"
+                >
                   Sign in
                 </button>
               </div>
             </form>
 
             {/* Sign Up Link */}
-          <div className="text-center text-sm text-gray-600 mt-12">
-            Don't have an account?{' '}
-            <a href="/signup" className="text-black underline font-medium hover:underline">
-              Create an account
-            </a>
-          </div>
+            <div className="text-center text-sm text-gray-600 mt-12">
+              Don't have an account?
+              <a
+                href="/signup"
+                className="text-black underline font-medium hover:underline"
+              >
+                Create an account
+              </a>
+            </div>
 
-          {/* OR Divider */}
-          <div className="flex items-center my-4">
-            <hr className="flex-grow border-gray-300" />
-            <span className="mx-4 text-sm text-gray-500">OR</span>
-            <hr className="flex-grow border-gray-300" />
-          </div>
+            {/* OR Divider */}
+            <div className="flex items-center my-4">
+              <hr className="flex-grow border-gray-300" />
+              <span className="mx-4 text-sm text-gray-500">OR</span>
+              <hr className="flex-grow border-gray-300" />
+            </div>
 
-          {/* Social Login Buttons */}
-          <div className="flex justify-center space-x-4">
-            <button className="p-2">
-              <img src='/facebook.svg' alt='facebook' />
-            </button>
-            <button className="p-2">
-            <img src='/google.svg' alt='gooogle' />
-            </button>
-          </div>
+            {/* Social Login Buttons */}
+            <div className="flex justify-center space-x-4">
+              <button className="p-2">
+                <img src="/facebook.svg" alt="facebook" />
+              </button>
+              <button className="p-2">
+                <img src="/google.svg" alt="gooogle" />
+              </button>
+            </div>
 
-          {/* Terms and Privacy */}
-          <div className="text-xs text-gray-500 text-center my-12">
-            By continuing, you confirm you are 18 or over and agree to our{' '}
-            <a href="#" className="underline">
-              Privacy Policy
-            </a>{' '}
-            and{' '}
-            <a href="#" className="underline">
-              Terms of Use
-            </a>.
-          </div>
+            {/* Terms and Privacy */}
+            <div className="text-xs text-gray-500 text-center my-12">
+              By continuing, you confirm you are 18 or over and agree to our{" "}
+              <a href="#" className="underline">
+                Privacy Policy
+              </a>{" "}
+              and{" "}
+              <a href="#" className="underline">
+                Terms of Use
+              </a>
+              .
+            </div>
           </div>
         </div>
       </AuthLayout>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import AuthLayout from '../components/AuthLayout'
-
-
+import React from "react";
+import AuthLayout from "../components/AuthLayout";
 
 const SignupPage = () => {
   return (
     <div>
       <AuthLayout>
         {/* Left Section with Background */}
-        <div className='flex-1'>
-          <div className='flex flex-col h-5/6 justify-center items-center'>
+        <div className="flex-1">
+          <div className="flex flex-col h-5/6 justify-center items-center">
             <h1 className="text-white text-5xl mt-24 font-bold px-8">
-            Welcome to Urban Spacer® – Transforming  Real Estate Solutions!
+              Welcome to Urban Spacer® – Transforming Real Estate Solutions!
             </h1>
           </div>
         </div>
 
         {/* Right section with Background */}
         <div className="flex-1 flex items-center justify-center bg-red text-black">
-          <div className='bg-white w-2/3 flex flex-col px-16 rounded-2xl mt-12'>
-            <h1 className='mt-6'>Create a free account</h1>
-            <p>Sign up to access tailored services for your real estate needs</p>
+          <div className="bg-white w-2/3 flex flex-col px-16 rounded-2xl mt-12">
+            <h1 className="mt-6">Create a free account</h1>
+            <p>
+              Sign up to access tailored services for your real estate needs
+            </p>
             <form className="mt-6 space-y-4">
               <div>
-                <label htmlFor="fullname" className="block text-sm font-medium text-gray-700">
+                <label
+                  htmlFor="fullname"
+                  className="block text-sm font-medium text-gray-700"
+                >
                   Fullname
                 </label>
                 <input
@@ -33,8 +36,11 @@ const SignupPage = () => {
                 />
               </div>
 
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
                   Email address
                 </label>
                 <input
@@ -46,9 +52,12 @@ const SignupPage = () => {
 
               {/* Password Input */}
               <div>
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                    Password
-                  </label>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
                 <input
                   type="password"
                   id="password"
@@ -57,58 +66,68 @@ const SignupPage = () => {
               </div>
 
               <div>
-                <p>Use 8 or more characters with a mix of letters, numbers and symbols. Must not contain your name or username.</p>
+                <p>
+                  Use 8 or more characters with a mix of letters, numbers and
+                  symbols. Must not contain your name or username.
+                </p>
               </div>
 
               {/* Sign In Button */}
               <div>
-                <button type="submit" className="w-full mt-6 py-3 rounded-full px-4 bg-[rgba(33,33,33,1)] text-white">
+                <button
+                  type="submit"
+                  className="w-full mt-6 py-3 rounded-full px-4 bg-[rgba(33,33,33,1)] text-white"
+                >
                   Create Account
                 </button>
               </div>
             </form>
 
             {/* Sign Up Link */}
-          <div className="text-center text-sm text-gray-600 mt-12">
-            Don't have an account?{' '}
-            <a href="/signup" className="text-black underline font-medium hover:underline">
-              Create an account
-            </a>
-          </div>
+            <div className="text-center text-sm text-gray-600 mt-12">
+              Don't have an account?
+              <a
+                href="/signup"
+                className="text-black underline font-medium hover:underline"
+              >
+                Create an account
+              </a>
+            </div>
 
-          {/* OR Divider */}
-          <div className="flex items-center my-4">
-            <hr className="flex-grow border-gray-300" />
-            <span className="mx-4 text-sm text-gray-500">OR</span>
-            <hr className="flex-grow border-gray-300" />
-          </div>
+            {/* OR Divider */}
+            <div className="flex items-center my-4">
+              <hr className="flex-grow border-gray-300" />
+              <span className="mx-4 text-sm text-gray-500">OR</span>
+              <hr className="flex-grow border-gray-300" />
+            </div>
 
-          {/* Social Login Buttons */}
-          <div className="flex justify-center space-x-4">
-            <button className="p-2">
-              <img src='/facebook.svg' alt='facebook' />
-            </button>
-            <button className="p-2">
-            <img src='/google.svg' alt='gooogle' />
-            </button>
-          </div>
+            {/* Social Login Buttons */}
+            <div className="flex justify-center space-x-4">
+              <button className="p-2">
+                <img src="/facebook.svg" alt="facebook" />
+              </button>
+              <button className="p-2">
+                <img src="/google.svg" alt="gooogle" />
+              </button>
+            </div>
 
-          {/* Terms and Privacy */}
-          <div className="text-xs text-gray-500 text-center my-12">
-            By continuing, you confirm you are 18 or over and agree to our{' '}
-            <a href="#" className="underline">
-              Privacy Policy
-            </a>{' '}
-            and{' '}
-            <a href="#" className="underline">
-              Terms of Use
-            </a>.
-          </div>
+            {/* Terms and Privacy */}
+            <div className="text-xs text-gray-500 text-center my-12">
+              By continuing, you confirm you are 18 or over and agree to our
+              <a href="#" className="underline">
+                Privacy Policy
+              </a>
+              and
+              <a href="#" className="underline">
+                Terms of Use
+              </a>
+              .
+            </div>
           </div>
         </div>
       </AuthLayout>
     </div>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
